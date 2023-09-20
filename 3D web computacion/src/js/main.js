032d3d5ec6ef03b390eabed3970c460566850a65
Nicolás Echeverri <moque1987@gmail.com>
2023-09-20 11:14:20 -0500

    Modelos cargados

--- 3D web computacion/src/js/main.js
+++ 3D web computacion/src/js/main.js
@@ -1,13 +1,13 @@
 /*Autor: Nicolás Toro Echeverri
 Fecha de creación: 23/08/2023 9:33 am
-Ultima modificación:31/08/2023 10:21 pm */
+Ultima modificación:30/08/2023 9:22 am */
 
 var scene = null,
     camera = null,
     renderer = null,
     controls= null;
     /*cube= null,torus = null, mesh = null*/
-    var geometry, material, mesh, figures= [], light=null;
+    var geometry, material, mesh, figures= [];
 
     const size= 30,
     divisions = 30;
@@ -16,7 +16,7 @@ var scene = null,
         //Scene, camera, renderer
 
         scene = new THREE.Scene();
-        scene.background = new THREE.Color(0x151718);
+        scene.background = new THREE.Color(0xC8C8C8);
         camera = new THREE.PerspectiveCamera( 
             75,                                 // ángulo de visión (abajo o arriba)
             window.innerWidth / window.innerHeight,   // relación de aspecto 16:9
@@ -32,6 +32,30 @@ var scene = null,
         controls= new THREE.OrbitControls(camera,renderer.domElement);
         camera.position.set(0,10,1);
         controls.update();
+
+       /* // ----OBJECTS----
+        // figura Cube
+        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        const material = new THREE.MeshBasicMaterial( { color: 0xFF0000, wireframe: true} );
+         cube = new THREE.Mesh( geometry, material );
+        scene.add( cube );
+
+        // figrua Torus
+        const geometrytorus = new THREE.TorusGeometry( 0.8, 0.1, 5, 20 ); 
+        const materialtorus = new THREE.MeshBasicMaterial( { color: 0xFF0000, wireframe: true } ); 
+        torus = new THREE.Mesh( geometrytorus, materialtorus ); 
+        scene.add( torus );
+        // figura mesh
+        const geometrymesh = new THREE.RingGeometry( 1, 4, 1 ); 
+        const materialmesh = new THREE.MeshBasicMaterial( { color: 0xFF0000, side: THREE.DoubleSide, wireframe: true } );
+         mesh = new THREE.Mesh( geometrymesh, materialmesh ); 
+        scene.add( mesh ); 
+
+        torus.position.x = -8;
+        mesh.position.x= 8;
+        cube.position.x=0;
+        
+        */
         
         camera.position.z = 7;
         //Grid Helper
@@ -44,41 +68,7 @@ var scene = null,
         const axesHelper = new THREE.AxesHelper(5);
         scene.add(axesHelper);
 
-        createLight("ambient");
-        // createLight("spotLight");
-        createLight("pointLight");
         animate();
-    }
-
-    //Function createLight -- crea las luces
-
-    function createLight(typeLight){
-
-        switch (typeLight) {
-            case "ambient":
-                light = new THREE.AmbientLight( 0xffffff); // soft white light
-                scene.add( light );
-              break;
-            case "pointLight":
-                light = new THREE.PointLight( 0xffffff, 1, 100 );
-                light.position.set( 0, 10, 0 );
-                scene.add( light );
-    
-                const sphereSize = 1;
-                const pointLightHelper = new THREE.PointLightHelper( light, sphereSize );
-               // scene.add( pointLightHelper );
-              break;
-            case "spotLight":
-                light = new THREE.SpotLight( 0xffffff);
-                light.position.set( 10, 10, 10 );
-                scene.add( light );
-    
-                // const spotLightHelper = new THREE.SpotLightHelper( light );
-               // scene.add( spotLightHelper );
-              break;
-          }
-
-
     }
 
  //function createGeometry -- switch case (cube,torus,mesh), que genere la figura en un lugar random
@@ -87,84 +77,30 @@ var scene = null,
     
     switch (shape){
     case 'cube':
-        const texture = new THREE.TextureLoader().load('../images/animals/face1.jpg');
-        var materialCube = [new THREE.MeshBasicMaterial ({map:new THREE.TextureLoader().load('../images/animals/face1.jpg')}),
-                                new THREE.MeshBasicMaterial ({map:new THREE.TextureLoader().load('../images/animals/face2.png')}),
-                                new THREE.MeshBasicMaterial ({map:new THREE.TextureLoader().load('../images/animals/face3.jpg')}),
-                                new THREE.MeshBasicMaterial ({map:new THREE.TextureLoader().load('../images/animals/face4.jpg')}),
-                                new THREE.MeshBasicMaterial ({map:new THREE.TextureLoader().load('../images/animals/face5.png')}),
-                                new THREE.MeshBasicMaterial ({map:new THREE.TextureLoader().load('../images/animals/face6.jpg')})];
         geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        material = new THREE.MeshBasicMaterial({ color: 0xffffff, 
-            transparent: true,
-            opacity: 1,
-            side: THREE.DoubleSide,
-            map:texture,
-            wireframe: false});
-        mesh = new THREE.Mesh(geometry, materialCube);
-     
         break;
 
     case 'torus':
-         geometry = new THREE.TorusGeometry( 1, 0.15, 120, 50 ); 
-         material = new THREE.MeshPhongMaterial({ color: 0xf2E103A,
-            transparent:true,
-            specular: 0xf102B3A,
-            opacity:1});
-            mesh = new THREE.Mesh(geometry, material);
+         geometry = new THREE.TorusGeometry( 0.8, 0.1, 5, 20 ); 
         break;
 
-    case 'cone':
-        geometry = new THREE.ConeGeometry( 1, 2, 50 ); 
-        material = new THREE.MeshPhongMaterial( {color: 0xf102B3A,
-        transparent:true,
-        specular: 0xf2E103A,
-        opacity: 1} );
-        mesh = new THREE.Mesh(geometry, material ); scene.add( mesh);
+    case 'ring':
+        geometry = new THREE.RingGeometry( 1, 1.5, 1 ); 
         break;
-
 
         default:
             console.error('Figura no encontrada');
             return;
      }
 
-    const x = (Math.random() - 0.5) * size;
-    const y = Math.random() * 5;
-    const z = (Math.random() - 0.5) * size;
-
-    mesh.position.set(x, y, z);
-    scene.add(mesh);
-
-    figures.push(mesh);
-     
-        /* const texture = new THREE.TextureLoader().load('../images/animals/face1.jpg' ); 
-    
-        material = [new THREE.MeshStandardMaterial({color: 0xFFFFFF, 
-            map:texture,
-            side: THREE.DoubleSide,
-            transparent: true,
-            opacity: 1, 
-            wireframe: false,
-            wireframeLinewidth: 6}),
-            new THREE.MeshStandardMaterial ({map:new THREE.TextureLoader().load('../images/animals/face1.jpg')}),
-            new THREE.MeshStandardMaterial ({map:new THREE.TextureLoader().load('../images/animals/face2.png')}),
-            new THREE.MeshStandardMaterial ({map:new THREE.TextureLoader().load('../images/animals/face3.jpg')}),
-            new THREE.MeshStandardMaterial ({map:new THREE.TextureLoader().load('../images/animals/face4.jpg')}),
-            new THREE.MeshStandardMaterial ({map:new THREE.TextureLoader().load('../images/animals/face5.png')}),
-            new THREE.MeshStandardMaterial ({map:new THREE.TextureLoader().load('../images/animals/face6.jpg')})];
-
-        
-        
-        
-            
+        material = new THREE.MeshBasicMaterial({color: 0xFF0000, wireframe: true});
         mesh = new THREE.Mesh(geometry,material);
         mesh.position.x = (Math.random() -  0.5)* size ;
         mesh.position.z = (Math.random() -  0.5)* size ;
         scene.add (mesh);
 
         figures.push(mesh);
- */
+
        
    }
 
@@ -177,20 +113,9 @@ var scene = null,
     for(let i=0; i<figures.length; i++){  /* Se creó un arreglo con la variable figures=[] para almacenar la animacion de la figura anterior y así no se detenga (arreglo) */
         figures[i].rotation.x += 0.03;
         figures[i].rotation.y += 0.03;
-     
     }
     
     }
-
-    function clearScene() {
-        // Remove all shapes from the scene.
-        scene.children.forEach(child => {
-            if (child instanceof THREE.Mesh) {
-                scene.remove(child);
-            }
-        });
-    }
-    
 
    
     window.addEventListener( 'resize', onWindowResize, false );
---
+++ 3D web computacion/src/js/mainObjects.js
@@ -0,0 +1,128 @@
+var scene = null,
+    camera = null,
+    renderer = null,
+    controls= null;
+
+    const size= 30,
+    divisions = 30;
+
+    function startScene() {
+        //Scene, camera, renderer
+
+        scene = new THREE.Scene();
+        scene.background = new THREE.Color(0xC8C8C8);
+        camera = new THREE.PerspectiveCamera( 
+            75,                                 // ángulo de visión (abajo o arriba)
+            window.innerWidth / window.innerHeight,   // relación de aspecto 16:9
+            0.1,                                      // mas cerca(no renderiza)
+            1000 );                                   // mas lejos(no renderiza)
+        
+        renderer = new THREE.WebGLRenderer({canvas: document.getElementById('model')});
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        document.body.appendChild( renderer.domElement );
+    
+        //ORBIT CONTROLS
+        controls= new THREE.OrbitControls(camera,renderer.domElement);
+        camera.position.set(0,10,1);
+        controls.update();
+
+        
+        camera.position.z = 7;
+        //Grid Helper
+        //const gridHelper = new THREE.GridHelper( size, divisions );
+        //scene.add(gridHelper);
+
+        //Axes helper
+        //const axesHelper = new THREE.AxesHelper(5);
+        //scene.add(axesHelper);
+
+        const rende = new THREE.WebGLRenderer();
+rende.shadowMap.enabled = true;
+rende.shadowMap.type = THREE.PCFSoftShadowMap; // default THREE.PCFShadowMap
+
+//Create a PointLight and turn on shadows for the light
+const light = new THREE.PointLight( 0xffffff, 1, 100 );
+light.position.set( 0, 10, 4 );
+light.castShadow = true; // default false
+scene.add( light );
+
+//Set up shadow properties for the light
+light.shadow.mapSize.width = 512; // default
+light.shadow.mapSize.height = 512; // default
+light.shadow.camera.near = 0.5; // default
+light.shadow.camera.far = 500; // default
+
+const lightt = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( lightt );
+
+
+        
+        //const light = new THREE.AmbientLight( 0xFFFFFF ); // soft white light
+        //scene.add( light );
+
+
+        animate();
+         // Escenario
+        loadModel_objMtl("../models/obj_mtl/escenario/","Escenario.obj","Escenario.mtl");
+    
+
+    }
+
+    function animate(){
+
+        requestAnimationFrame(animate);
+        controls.update();
+        renderer.render(scene,camera);
+        
+        }
+
+     window.addEventListener( 'resize', onWindowResize, false );
+
+    function onWindowResize(){
+
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize( window.innerWidth, window.innerHeight );
+
+}
+
+function loadModel_objMtl(){
+    // Load MTL
+    var mtlLoader = new THREE.MTLLoader();
+    mtlLoader.setResourcePath("../models/obj_mtl/escenario/");
+    mtlLoader.setPath("../models/obj_mtl/escenario/");
+    mtlLoader.load("Escenario.mtl", function (materials) {
+        materials.preload();
+
+        // Load OBJ
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setPath("../models/obj_mtl/escenario/");
+        objLoader.setMaterials(materials);
+        objLoader.load("Escenario.obj", function (object) {
+            scene.add(object);
+            // object.scale.set(3,3,3);
+        });
+    });
+
+
+
+
+    // Load gtlf
+    var mtlLoader = new THREE.MTLLoader();
+    mtlLoader.setResourcePath("../models/gltf/Personaje/Duck.gltf");
+    mtlLoader.setPath("../models/obj_mtl/Personaje/");
+    mtlLoader.load("Duck.gltf", function (materials) {
+        materials.preload();
+
+        // Load OBJ
+        var objLoader = new THREE.OBJLoader();
+        objLoader.setPath("../models/Personaje/Duck0.bin");
+        objLoader.setMaterials(materials);
+        objLoader.load("Duck0.bin", function (object) {
+            scene.add(object);
+            // object.scale.set(3,3,3);
+        });
+    });
+}
