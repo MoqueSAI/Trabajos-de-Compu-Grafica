7eb2f2175a4231c12fe13dd446e8c062aa4126fd
Nicolás Echeverri <moque1987@gmail.com>
2023-09-27 10:11:06 -0500

    Sound

--- 3D web computacion/src/js/mainObjects.js
+++ 3D web computacion/src/js/mainObjects.js
@@ -65,7 +65,9 @@ scene.add( lightt );
         animate();
          // Escenario
         loadModel_objMtl("../models/obj_mtl/escenario/","Escenario.obj","Escenario.mtl");
-    
+         // Duck Model
+    loadGltf('../models/gltf/other/', 'Duck.gltf');
+
 
     }
 
@@ -110,16 +112,59 @@ function loadModel_objMtl(){
 
     //load GLTF
   
-    const gltfLoader = new THREE.GLTFLoader();
-gltfLoader.load('../models/gltf/other/Duck.gltf', (gltf) => {   
-    const duck = gltf.scene; // El objeto 3D del pato
+    //const gltfLoader = new THREE.GLTFLoader();
+//gltfLoader.load('../models/gltf/other/Duck.gltf', (gltf) => {   
+    //const duck = gltf.scene; // El objeto 3D del pato
 
     // position
-    const newPosition = new THREE.Vector3(0, 0, -3); // Cambia x, y, z a las coordenadas deseadas
+    //const newPosition = new THREE.Vector3(0, 0, -3); // Cambia x, y, z a las coordenadas deseadas
 
     // Aplly the position 
-    duck.position.copy(newPosition);
+    //duck.position.copy(newPosition);
 
-    scene.add(duck);
-});
-    
+    //scene.add(duck);
+//});
+
+function loadGltf(path, nameGltfGet) {
+    var nameGltf = path + nameGltfGet;
+
+    // Instantiate a loader
+    const loader = new THREE.GLTFLoader();
+
+    // Optional: Provide a DRACOLoader instance to decode compressed mesh data
+    const dracoLoader = new THREE.DRACOLoader();
+    dracoLoader.setDecoderPath(path);
+    loader.setDRACOLoader(dracoLoader);
+
+    // Load a glTF resource
+    loader.load(
+        // resource URL
+        nameGltf,
+        // called when the resource is loaded
+        function (gltf) {
+
+            scene.add(gltf.scene);
+
+            gltf.animations; // Array<THREE.AnimationClip>
+            gltf.scene; // THREE.Group
+            gltf.scenes; // Array<THREE.Group>
+            gltf.cameras; // Array<THREE.Camera>
+            gltf.asset; // Object
+
+            gltf.scene.position.set(0, 0, -3);
+
+        },
+        // called while loading is progressing
+        function (xhr) {
+
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+        },
+        // called when loading has errors
+        function (error) {
+
+            console.log('An error happened');
+
+        }
+    );
+}
